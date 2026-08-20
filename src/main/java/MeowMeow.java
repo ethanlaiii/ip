e68b42b883df4cf6e87e9a1615a2f4f9f3bfcf5c
Lai Mun Weng Ethan <ethanlmw04@gmail.com>
2026-08-21 05:03:03 +0800

    Rename main class and add greeting

---
+++ src/main/java/MeowMeow.java
@@ -0,0 +1,16 @@
+public class MeowMeow {
+    public static void main(String[] args) {
+        String logo = "  /\\_/\\\n"
+                + " ( o.o )\n"
+                + "  > ^ <\n";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println("Hello! I'm MeowMeow.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Byebye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
