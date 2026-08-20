6b17da20b02f77bf4857ed7bd423c65654f25d1d
Lai Mun Weng Ethan <ethanlmw04@gmail.com>
2026-08-21 06:25:07 +0800

    Add ability to mark tasks as done

--- src/main/java/MeowMeow.java
+++ src/main/java/MeowMeow.java
@@ -13,7 +13,7 @@ public class MeowMeow {
         System.out.println("What can I do for you? Meow :>");
         System.out.println(line);
 
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -28,12 +28,27 @@ public class MeowMeow {
                 break;
             } else if (input.equals("list")) {
                 System.out.println(line);
+                System.out.println("     Meow :> Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("     " + (i + 1) + ". " + taskList[i]);
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                taskList[index].markAsDone();
+                System.out.println(line);
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       " + taskList[index]);
+                System.out.println(line);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                taskList[index].markAsNotDone();
+                System.out.println(line);
+                System.out.println("     OK, I've marked this task as not done yet:");
+                System.out.println("       " + taskList[index]);
+                System.out.println(line);
             } else {
-                taskList[taskCount] = input;
+                taskList[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(line);
                 System.out.println("     Meow :> added: " + input);
