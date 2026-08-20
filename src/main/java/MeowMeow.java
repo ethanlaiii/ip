90278f191908197a0ce3c7b82d0e31879507517b
Lai Mun Weng Ethan <ethanlmw04@gmail.com>
2026-08-21 05:57:32 +0800

    Add functionality for storing and listing tasks

--- src/main/java/MeowMeow.java
+++ src/main/java/MeowMeow.java
@@ -13,18 +13,30 @@ public class MeowMeow {
         System.out.println("What can I do for you? Meow :>");
         System.out.println(line);
 
+        String[] taskList = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("     Bye. Hope to see you again soon! Meow :>");
                 System.out.println(line);
                 break;
-            } else {
+            } else if (input.equals("list")) {
                 System.out.println(line);
-                System.out.println("     " + input);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("     " + (i + 1) + ". " + taskList[i]);
+                }
+                System.out.println(line);
+            } else {
+                taskList[taskCount] = input;
+                taskCount++;
+                System.out.println(line);
+                System.out.println("     Meow :> added: " + input);
                 System.out.println(line);
             }
         }
