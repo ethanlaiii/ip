21eef0c25450ccaea259528fdbf9a2da1997fd44
Lai Mun Weng Ethan <ethanlmw04@gmail.com>
2026-09-03 04:11:47 +0800

    Add GUI dialog and main window components

--- src/main/java/meowmeow/DialogBox.java
+++ src/main/java/meowmeow/DialogBox.java
@@ -1,4 +1,74 @@
 package meowmeow;
 
-public class DialogBox {
+import java.io.IOException;
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
+ */
+public class DialogBox extends HBox {
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
+
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.setText(text);
+        displayPicture.setImage(img);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    /**
+     * Returns a dialog box representing the user's message.
+     *
+     * @param text Text to display in the dialog box.
+     * @param img Image to display as the speaker's face.
+     * @return A dialog box aligned for the user.
+     */
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
+    }
+
+    /**
+     * Returns a dialog box representing MeowMeow's reply, with the image on the left.
+     *
+     * @param text Text to display in the dialog box.
+     * @param img Image to display as the speaker's face.
+     * @return A flipped dialog box aligned for MeowMeow.
+     */
+    public static DialogBox getMeowMeowDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
+        db.flip();
+        return db;
+    }
 }
--- src/main/java/meowmeow/MainWindow.java
+++ src/main/java/meowmeow/MainWindow.java
@@ -1,4 +1,57 @@
 package meowmeow;
 
-public class MainWindow {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private MeowMeow meowMeow;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Meow2.JPG"));
+    private Image meowMeowImage = new Image(this.getClass().getResourceAsStream("/images/Meow1.jpg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /**
+     * Injects the MeowMeow instance.
+     *
+     * @param m Chatbot instance to route user input to.
+     */
+    public void setMeowMeow(MeowMeow m) {
+        meowMeow = m;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing MeowMeow's reply and then appends
+     * them to the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = meowMeow.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getMeowMeowDialog(response, meowMeowImage)
+        );
+        userInput.clear();
+    }
 }
